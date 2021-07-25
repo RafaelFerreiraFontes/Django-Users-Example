b8c6621787f0dff96688fca684fc36c54ac4a7f2
Rafael Ferreira Fontes <50003237+RafaelFerreiraFontes@users.noreply.github.com>
2021-07-25 13:00:58 -0300

    Update .env, add DB_URI

--- mongoUltis.py
+++ mongoUltis.py
@@ -6,14 +6,16 @@ def get_db():
     
     env = environ.Env(
         DB_USER=(str, ""),
-        DB_PASS=(str, "")
+        DB_PASS=(str, ""),
+        DB_URI=(str,"")
     )
 
     environ.Env.read_env()
 
     DB_USER = env('DB_USER')
     DB_PASS = env('DB_PASS')
+    DB_URI = env('DB_URI')
 
-    client = MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASS}@example-users.tmf25.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")    
+    client = MongoClient(DB_URI.format(DB_USER, DB_PASS))    
 
     return client
